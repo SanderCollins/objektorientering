54dbe733281adf7f4fc505d8d91acb0760f7e1d9
Sander Collins <116351079+SanderCollins@users.noreply.github.com>
2022-11-09 14:47:26 +0100

    Update bilregister.py

--- bilregister.py
+++ bilregister.py
@@ -1,51 +1,88 @@
-class Car():
-    '''
-    En klass som håller reda på några egenskaper hos en bil.
-    '''
-    # Metoden __init__, körs alltid då ett objekt skapas
+my_cars = []
+my_mileage = []
+my_brands = []
+my_color = []
 
+
+class Car:
     def __init__(self, brand, color, mileage):
-        # Nedanstående variabler kallas för attribut.
-        # Alla objekt av klassen Car har egna värden på dessa.
         self.brand = brand
         self.color = color
         self.mileage = mileage
 
     def get_brand(self):
-        '''
-        Skriver ut bilmärket
-        '''
         print(self.brand)
 
     def set_brand(self, new_brand):
-        '''
-        Parameter: new_brand | sträng
-        Uppdaterar bilmärket om det existerar. Om det inte existerar
-        så tilldelas aktuellt objekt märket enligt parametern.
-        '''
         self.brand = new_brand
 
     def get_color(self):
         print(self.color)
 
-
     def get_mileage(self):
         print(self.mileage)
 
+    def set_color(self, new_color):
+        self.color = new_color
+
+    def set_mileage(self, new_mileage):
+        self.mileage = new_mileage
 
 
-# ----------Huvudprogram----------
-# Nu när klassen finns kan vi skapa objekt (variabler) med denna typ.
-# Dessa objekt har också tillgång till klassens metoder (funktioner).
-a_car = Car('', 'Blå', 1587)
-a_car.get_brand()
-a_car.set_brand('')
-a_car.get_brand()
-b_car = Car('Lamborghini', 'vit', 199999999)
-b_car.get_brand()       # får vi märket på den nya bilen
-b_car.get_color()       # får vi färgen
-b_car.get_mileage()     #får vi miltalet
-b_car.set_brand("Ferrari")       # får vi nu ett nytt bilmärke
-b_car.get_brand()       # får vi bilmärket igen
+for bilar in my_cars:
+    print(bilar)
 
 # ----------Huvudprogram----------
+a_car = Car('porsche', 'Blå', 1587)
+a_car.get_brand()
+a_car.set_brand("volvo")
+a_car.get_brand()
+b_car = Car('Lamborghini', 'vit', 199)
+b_car.get_brand()
+b_car.get_color()
+b_car.get_mileage()
+c_car = Car('ferrari', 'röd', 1032)
+c_car.get_brand()
+c_car.get_color()
+c_car.get_mileage()
+d_car = Car('bentley', 'silver', 487)
+d_car.get_brand()
+d_car.get_color()
+d_car.get_mileage()
+
+my_cars.append(a_car)
+my_cars.append(b_car)
+my_cars.append(c_car)
+my_cars.append(d_car)
+
+
+#inte klar riktigt här 
+
+
+def meny():
+    svar = input("1 för att se märke, 2 för att se färg, 3 för att se miltal")
+    sorted_list = []
+    if svar == "1":
+        for car in my_cars:
+            sorted_list.append(car.get_brand)
+        sorted_list.sort()
+        for car in sorted_list:
+            print(car)
+    elif svar == "2":
+        for car in my_cars:
+            sorted_list.append(car.get_color)
+        sorted_list.sort()
+        for car in sorted_list:
+            print(car)
+    elif svar == "3":
+        for car in my_cars:
+            sorted_list.append(car.get_mileage)
+        sorted_list.sort()
+        for car in sorted_list:
+            print(car)
+    else:
+        print("du är fan dum i huvudet")
+
+
+meny()
+
